63d51c6ed65b31b0c5814b83b878f358d104290c
Linus Henke <linus.henke@enke5.de>
2024-07-29 13:12:51 +0200

    Repo refactoring.

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ then
 fi
 
 
+# Setting up some variables needed to create mailMan as instance for this user
 CURRENT_DIR=$(pwd)
 CURRENT_USER=$(whoami)
 CURRENT_HOME=$(echo ~)
@@ -24,6 +25,11 @@ mkdir $CURRENT_HOME/.mailMan
 
 sudo cp target/release/mailMan_imap_service /usr/local/bin
 sudo cp mailMan_$CURRENT_USER.service /etc/systemd/system/
+
+
+cd ../cli
+cargo build --release
+sudo cp target/release/mailMan_cli /usr/local/bin
 
 
 cd $CURRENT_DIR
